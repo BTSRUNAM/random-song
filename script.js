ebf9eb572c1572aac4a20918b69b0c23b66c2443
BTSRUNAM <Kiriasd@outlook.com>
2024-11-09 16:14:00 -0300

    Actualizacion del diseño de la pagina

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 function mostrarCancion() {
     // Lista de canciones
     const canciones = [
-        "Song 1 - Artista 1",
-        "Song 2 - Artista 2",
-        "Song 3 - Artista 3",
-        "Song 4 - Artista 4",
-        "Song 5 - Artista 5"
-    ];
+        { name: "Blood Sweat and tears", artist: "BTS", link: "https://music.apple.com/ar/album/blood-sweat-tears/1596532477?i=1596532481" },
+        { name: "Best of me", artist: "BTS", link: "https://music.apple.com/ar/album/best-of-me/1596529064?i=1596529067" },
+        { name: "Take Two", artist: "BTS", link: "https://music.apple.com/ar/album/take-two/1690421569?i=1690421575" },
+        { name: "Boy with luv ft. Halsey", artist: "BTS", link: "https://music.apple.com/ar/album/boy-with-luv-feat-halsey/1599171924?i=1599172208" },
+        { name: "Butterfly (prologue mix)", artist: "BTS", link: "https://music.apple.com/ar/album/butterfly-prologue-mix/1598661880?i=1598661900" },
+        { name: "Your eyes tell", artist: "BTS", link: "https://music.apple.com/ar/album/your-eyes-tell/1517969553?i=1517969731" },
+        { name: "Not today", artist: "BTS", link: "https://music.apple.com/ar/album/not-today/1596528839?i=1596529383" },
+        { name: "Look Here", artist: "BTS", link: "https://music.apple.com/ar/album/look-here/1597016856?i=1597017528" },
+        { name: "Like", artist: "BTS", link: "https://music.apple.com/ar/album/like/1596528414?i=1596528633" }
+      ];
+      
 
     // Seleccionar una canción aleatoria
     const indiceAleatorio = Math.floor(Math.random() * canciones.length);
